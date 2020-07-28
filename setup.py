a494e2cf2ebe2af80e7e6f4e6be04bac94a40f5d
Vin985 <sylvain.christin@gmail.com>
2020-07-28 17:28:45 -0400

    First commit: JumpSlider

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    author="Vin985",
+    description=("Extra widgets for Qt and Python using PySide2"),
+    keywords="qt, python, PySide2, widgets",
+    # long_description=read('README.md'),
+    name="pyqt_extra",
+    version="0.1",
+    packages=find_packages(),
+    package_data={"": ["*.svg", "*.yaml", "*.zip", "*.ico", "*.bat"]},
+)
